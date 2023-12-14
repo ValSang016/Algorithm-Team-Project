89cad542b9cc598ab686294b8bc8901d4c3cef31
ValSang016 <tkdgjs1315@naver.com>
2023-12-14 13:18:26 +0900

    Last step / v.2.2

--- app/src/main/java/com/gachon/algorithm/Activity3.java
+++ app/src/main/java/com/gachon/algorithm/Activity3.java
@@ -72,6 +72,7 @@ public class Activity3 extends AppCompatActivity {
         public FetchDataAsyncTask(ManageInfo manageInfo) {
             this.manageInfo = manageInfo;
         }
+
         @Override
         protected List<User> doInBackground(Void... voids) {
             // 백그라운드에서 데이터베이스에서 데이터 가져오기
@@ -102,7 +103,7 @@ public class Activity3 extends AppCompatActivity {
             addTableHeader(cancelledTableLayout, "Room Number", "Cancelled Group Name", "Start Time", "End Time");
 
             for (User user : cancelledUsers) {
-                addTableRow(cancelledTableLayout,0, user.getGroup_name(), formatTime(user.getStart_hour(), user.getStart_minute()), formatTime(user.getEnd_hour(), user.getEnd_minute()));
+                addTableRow(cancelledTableLayout, 0, user.getGroup_name(), formatTime(user.getStart_hour(), user.getStart_minute()), formatTime(user.getEnd_hour(), user.getEnd_minute()));
             }
         }
 
@@ -157,10 +158,13 @@ public class Activity3 extends AppCompatActivity {
 
         // 종료 시간을 기준으로 정렬하고, 가장 많은 예약 시간을 갖는 경우를 선택하는 알고리즘
         private List<User> sortUsers(List<User> users) {
-            List<User> finalReservation = new ArrayList<>();
-            List<User> currentUsers = new ArrayList<>(users);
+            List<User> finalReservation = new ArrayList<>(); // 최종 예약 리스트
+            List<User> currentUsers = new ArrayList<>(users); // 현재 예약 가능한 사용자 리스트
 
+            // 각 회의실에 대해 반복
             for (int i = 0; i < manageInfo.getRoomCount(); i++) {
+
+                // currentUsers를 종료 시간에 따라 오름차순 정렬
                 Collections.sort(currentUsers, new Comparator<User>() {
                     @Override
                     public int compare(User u1, User u2) {
@@ -171,42 +175,58 @@ public class Activity3 extends AppCompatActivity {
                 List<List<User>> subsets = new ArrayList<>();
                 subsets.add(new ArrayList<>());
 
+                // 현재 예약 가능한 각 사용자에 대해
                 for (User user : currentUsers) {
                     List<List<User>> newSubsets = new ArrayList<>();
+
+// Activity selection
+                    // 기존의 모든 부분집합에 대해
                     for (List<User> subset : subsets) {
+                        // 부분집합이 비어있거나, 마지막 예약의 종료 시간 + 청소 시간이 사용자의 시작 시간보다 이전이면
+                        // 새 부분집합을 생성하고 사용자를 추가
                         if (subset.isEmpty() || (subset.get(subset.size() - 1).getEnd_hour() * 60 + subset.get(subset.size() - 1).getEnd_minute() + manageInfo.getCleaningTime()) <= (user.getStart_hour() * 60 + user.getStart_minute())) {
                             List<User> newSubset = new ArrayList<>(subset);
                             newSubset.add(user);
                             newSubsets.add(newSubset);
                         }
                     }
+
+                    // 새로 생성된 모든 부분집합을 추가
                     subsets.addAll(newSubsets);
                 }
 
                 List<User> maxSubset = null;
                 int maxTime = 0;
+
+                // 모든 부분집합에 대해
                 for (List<User> subset : subsets) {
                     int time = 0;
+
+                    // 부분집합의 모든 예약에 대해 시간을 계산
                     for (User user : subset) {
                         time += (user.getEnd_hour() * 60 + user.getEnd_minute()) - (user.getStart_hour() * 60 + user.getStart_minute());
                     }
+
+                    // 만약 계산된 시간이 최대 시간보다 크면, 최대 시간과 최대 부분집합을 업데이트
                     if (time > maxTime) {
                         maxTime = time;
                         maxSubset = subset;
                     }
                 }
 
+                // 최대 부분집합이 null이 아니면, 각 사용자의 방 번호를 설정하고 최종 예약 리스트에 추가
                 if (maxSubset != null) {
                     for (User user : maxSubset) {
                         user.setRoomNumber(i + 1); // 방 번호 설정
                     }
                     finalReservation.addAll(maxSubset);
-                    currentUsers.removeAll(maxSubset);
+                    currentUsers.removeAll(maxSubset); // 최대 부분집합의 사용자를 currentUsers에서 제거
                 }
             }
-            cancelledUsers = currentUsers;
-            return finalReservation;
-        }
 
+            cancelledUsers = currentUsers; // 처리되지 않은 예약들을 취소된 예약 리스트에 추가
+            return finalReservation; // 최종 예약 리스트 반환
+
+        }
     }
 }
